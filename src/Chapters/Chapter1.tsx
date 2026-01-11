06cea2a6392e1fc86ee934df9ef16ea98f54859e
Your Name <tangshirong477@gmail.com>
2026-01-10 19:06:17 -0500

    disclaimer

--- src/Chapters/Chapter1.tsx
+++ src/Chapters/Chapter1.tsx
@@ -3,12 +3,14 @@ import Para101 from "../Paragraphs/Para101"
 import Para146 from "../Paragraphs/Para146"
 import ChapterTitle from "./ChapterTitle"
 import ChapterNavigation from "../components/ChapterNavigation"
+import Processing from "../components/Processing"
 
 const Chapter1 = () => {
   return (
     <div className="space-y-16">
         <ChapterTitle title="第一卷"/>
         <Para101 />
+        <Processing />
         <Para146 />
         <ChapterNavigation currentChapter={1} />
     </div>
